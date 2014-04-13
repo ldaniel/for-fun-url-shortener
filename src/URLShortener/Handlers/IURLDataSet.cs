54b28442d2ee857651765a016653bf01f662df21
ldaniel <contato@leandrodaniel.com>
2014-04-13 13:30:31 -0300

    First version

---
+++ src/URLShortener/Handlers/IURLDataSet.cs
@@ -0,0 +1,13 @@
+﻿using RaboURLShortner.Models;
+
+namespace RaboURLShortner.Handlers
+{
+    public interface IURLDataSet
+    {
+        int GetNextId();
+        URL GetUrlByID(string id);
+        string GetIdByOriginalURL(URL urldata);
+        bool OriginalUrlExists(string originalUrl);
+        void PersistUrl(URL urldata);
+    }
+}
